6ca94f01f9fe5a2d1a4f9684027c94f2a7641999
StronkColombia <fmartinezmorales1989@gmail.com>
2024-05-29 09:18:13 -0500

    optimizacion de codigo

--- obj/Debug/net7.0/APIPortalKiosco.Entities.AssemblyInfo.cs
+++ obj/Debug/net7.0/APIPortalKiosco.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIPortalKiosco.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a7fbfc9bffc62ec41811670586827cee07d8e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1cb6f26354932962e6e7d58610da076caf0e2a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIPortalKiosco.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIPortalKiosco.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
