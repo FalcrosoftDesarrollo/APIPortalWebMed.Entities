c6b8b300e49cb9fd1777782e8623489b8f98e7ad
StronkColombia <fmartinezmorales1989@gmail.com>
2024-07-10 17:14:49 -0500

    update

--- obj/Debug/net7.0/APIPortalKiosco.Entities.AssemblyInfo.cs
+++ obj/Debug/net7.0/APIPortalKiosco.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIPortalKiosco.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48a2429c17e786edde0f64056aec866a94b9d3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bcc8c67febc7facaa275570ed30b1755455dfc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIPortalKiosco.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIPortalKiosco.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
